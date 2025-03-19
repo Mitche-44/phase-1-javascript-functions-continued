0c2e7c1df9c811f00881c9c88e6c305e65c40344
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-03-19 12:47:52 +0300

    javascript-functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity= 'go to the office'){
+   return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+ function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+// Example usage:
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are !!!a dedicated programmer!!!"
+console.log(wrapAdjective("%")("a dedicated programmer")); // Output: "You are %a dedicated programmer%!"
+console.log(wrapAdjective()()); // Output: "You are *special*!"
